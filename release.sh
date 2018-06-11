d8d620115551d5ed5a50a4b93c54f396246fef48
Alexy Shelest <alexy.shelest@gmail.com>
2018-06-11 19:09:34 +0100

    add release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo 'packaging files ...'
+# release
+cd ~/insiderpy/lib/python3.6/site-packages/
+zip -r9 ~/insider.$1.zip *
+cd ~/th3sys/insider
+zip -g ~/insider.$1.zip companies.py
+zip -g ~/insider.$1.zip connectors.py
+zip -g ~/insider.$1.zip find.py
+zip -g ~/insider.$1.zip save.py
+zip -g ~/insider.$1.zip trading.py
+zip -g ~/insider.$1.zip utils.py
+aws s3 cp ~/insider.$1.zip s3://$2/releases/insider.$1.zip
