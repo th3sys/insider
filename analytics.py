61a835eddd2a35489a0db2f08235e44804975f0d
Alexy Shelest <alexy.shelest@gmail.com>
2018-07-11 08:49:07 +0100

    data cleansing

--- analytics.py
+++ analytics.py
@@ -9,12 +9,16 @@ class DecisionEngine:
         self.__notify = notify
 
     def ClusterBuying(self, df, date, count):
+        # filter out null
+        pd.options.mode.chained_assignment = None  # default='warn'
+        df['DATE'] = df['DATE'].str.strip()
+        df = df[df.DATE != '0000-00-00']
         df['DATE'] = pd.to_datetime(df['DATE'])
         df = df.sort_values(by='DATE')
 
         first = date.replace(day=1)
         lastMonth = first - timedelta(days=1)
-        fromDate = datetime(date.year, lastMonth.month, date.day)
+        fromDate = datetime(lastMonth.year, lastMonth.month, date.day)
         df = df[(df['DATE'] >= fromDate) & (df['DATE'] < date)]
         df['TYPE'] = df['TYPE'].str.strip()
         df = df[df['TYPE'] == 'P-Purchase']
