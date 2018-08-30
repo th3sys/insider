fb4048b11615d6ea90900b2e6248e1e1bf7396a8
Alexy Shelest <alexy.shelest@gmail.com>
2018-08-30 23:18:15 +0100

    don't use cloud logger

--- save.py
+++ save.py
@@ -48,12 +48,10 @@ async def main(loop, logger, items, today, requestId, chunk_id):
 def lambda_handler(event, context):
 
     level = logging.DEBUG if 'LOGGING_LEVEL' in os.environ and os.environ['LOGGING_LEVEL'] == 'DEBUG' else logging.INFO
-    if os.environ['DEPLOYMENT_MODE'] == 'LAMBDA':
-        logger = logging.getLogger()
-        logger.setLevel(level)
-        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s')
-    else:
-        logger = utils.CloudLogger(level)
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s')
 
     logger.info('event %s' % event)
     logger.info('context %s' % context)
