56530090db3a889d444257bf16625cbe086f18f6
Alexy Shelest <alexy.shelest@gmail.com>
2018-09-17 19:08:44 +0100

    add image

--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,12 @@ import json
 import utils
 import os
 
-with open("../events/check.json") as json_file:
+with open("../events/find.json") as json_file:
     test_event = json.load(json_file, parse_float=utils.DecimalEncoder)
 
 client = boto3.client('lambda')
-for x in [1,4,5,6,7,8,11,12,13,14,15,18,19,20,21,22,25,26,27,28,29]:
-    test_event['time'] = '2017-12-%sT20:20:00Z' % x
+for x in [2,3,6,7,8,9,10,13,14,15,16,17,20,21,22,23,24,27,28,29,30,31]:
+    test_event['time'] = '2018-03-%sT20:20:00Z' % x
     test_event['resources'] = ["arn:aws:events:us-east-1::rule/EOD3"]
     response = client.invoke(
         FunctionName=os.environ['func'],
